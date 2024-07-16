3753482ec98c42f237fc0c7f4baee04e9ddad055
Cesare Torchia <cesare@parkingmycar.it>
2024-07-17 00:47:18 +0200

    a bit of margins

--- lib/src/feature/add_collection/view/add_collection_page.dart
+++ lib/src/feature/add_collection/view/add_collection_page.dart
@@ -12,33 +12,45 @@ class AddCollectionPage extends HookWidget {
   Widget build(BuildContext context) {
     final nameController = useTextEditingController();
     final descriptionController = useTextEditingController();
+    final formKey = useMemoized(GlobalKey<FormState>.new);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Collection'),
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: nameController,
-            decoration: const InputDecoration(labelText: 'Name'),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: nameController,
+                autofocus: true,
+                decoration: const InputDecoration(labelText: 'Name'),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) =>
+                    value?.isEmpty ?? true ? 'Required' : null,
+              ),
+              TextFormField(
+                controller: descriptionController,
+                decoration: const InputDecoration(labelText: 'Description'),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  if (!(formKey.currentState?.validate() ?? false)) return;
+                  context.pop(
+                    BGOCollectionCompanion.insert(
+                      name: nameController.text,
+                      description: descriptionController.text,
+                    ),
+                  );
+                },
+                child: const Text('Add'),
+              ),
+            ],
           ),
-          TextField(
-            controller: descriptionController,
-            decoration: const InputDecoration(labelText: 'Description'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              if (nameController.text.isEmpty) return;
-              context.pop(
-                BGOCollectionCompanion.insert(
-                  name: nameController.text,
-                  description: descriptionController.text,
-                ),
-              );
-            },
-            child: const Text('Add'),
-          ),
-        ],
+        ),
       ),
     );
   }
